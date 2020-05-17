842d3a4c89305cf31ea6512783c3a98e7ba6459c
Bruce Tan <brucetansh@gmail.com>
2020-05-17 12:50:03 +1200

    Command cat implemented

---
+++ user/commands/cat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <sys/fcntl.h>
+
+
+int main(int argc, char *argv[]){
+    int fd, ret;
+    char buf[256];
+    if(argc < 2)
+        return -1;
+    fd = open(argv[1], O_RDONLY);
+    if(fd < 0){
+        perror("open");
+    }
+    while((ret = read(fd, buf, 256)) > 0){
+        write(1, buf, ret);
+    }
+    return 0;
+}
+
+
+
