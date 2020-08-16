06a637ef5d36734438f039f9a63f968801d800a0
Bruce Tan <brucetansh@gmail.com>
2020-08-16 16:31:10 +1200

    Add testing

--- user/commands/echo.c
+++ user/commands/echo.c
@@ -3,10 +3,12 @@
 #include <sys/fcntl.h>
 #include <string.h>
 
+char test[] = {0x1b, 0};
 char cls[] = {0x1b, 0x5b, 0x31, 0x3b, 0x31, 0x48, 0};
 char cls2[] = {0x1b, 0x5b, 0x32, 0x4a, 0};
 
 int main(int argc, char *argv[]){
+    printf("test %s", test);
     printf("%s", cls);
     printf("%s", cls2);
     if(argc < 2)
