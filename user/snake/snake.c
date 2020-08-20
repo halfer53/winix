c88b4c4876a83cfffa20e836ef22c523cb9c5e20
Bruce Tan <brucetansh@gmail.com>
2020-08-20 13:49:50 +1200

    Fixed bug in snake

--- user/snake/snake.c
+++ user/snake/snake.c
@@ -7,10 +7,12 @@
 #include <stdlib.h>
 
 #define NUM_COLS  (100)
-#define NUM_ROWS  (32)
+#define NUM_ROWS  (31)
 #define INITIAL_SNAKE_LEN   (3)
 #define INITAL_FOOD_NUM (5)
-#define SNAKE_HEAD(board)   (list_first_entry(&board->snake, struct point, list))
+#define SNAKE_CHAR  ('x')
+#define FOOD_CHAR   ('o')
+#define GET_SNAKE_HEAD(board)   (list_first_entry(&board->snake, struct point, list))
 
 #define SUCCESS (0)
 #define FAIL    (-1)
@@ -18,6 +20,7 @@
 #define FAIL_RIGHT   (-3)
 #define FAIL_DOWN   (-4)
 #define FAIL_UP   (-5)
+#define FAIL_EAT_SELF   (-6)
 
 
 struct point{
@@ -42,13 +45,51 @@ struct point* new_point(int x, int y){
     return po;
 }
 
-void delete_point(struct point* p){
-    free(p);
+struct point* new_food(){
+    struct point* p;
+    int x = rand() % NUM_COLS;
+    int y = rand() % NUM_ROWS;
+    return new_point(x, y);
 }
 
 void disable_cursor(){
     static char str[] = {0x1b, 0x5b ,0x3f ,0x32 ,0x35 ,0x6c, 0};
     printf("%s", str);
+}
+
+void int2str(int value, int i, char* output){
+    int j;
+    while(i){
+        j = (value / i) % 10;
+        *output++ = '0' + j;
+        i /= 10;
+    }
+}
+
+void draw_point(struct point* pos, char character){
+    static char draw_pos[] = {0x1b, 0x5b, 0x30, 0x30, 0x30, 0x3b, 0x30, 0x30, 0x30, 0x48, 0};
+    static char format[] = "%s ";
+    char buf[2];
+    char *draw_format = draw_pos;
+    int2str(pos->y, 100, draw_format + 2);
+    int2str(pos->x, 100, draw_format + 6);
+    format[2] = character;
+    printf(format, draw_pos);
+    buf[0] = character;
+    buf[1] = '\0';
+    // fprintf(stderr, "Draw %s at x %d y %d | ", buf, pos->x, pos->y);
+}
+
+void clear_point(struct point* p){
+    draw_point(p, ' ');
+}
+
+void clear_screen(){
+    // byte stream for \e[2J
+    // static char cls[] = {0x1b, 0x5b, 0x31, 0x3b, 0x31, 0x48, 0x1b, 0x5b, 0x32, 0x4a, 0};
+    static char cls[] = {0x1b, 0x5b, 0x32, 0x4a, 0};
+
+    printf("%s", cls);
 }
 
 void board_init(struct board_struct *board){
@@ -63,30 +104,22 @@ void board_init(struct board_struct *board){
     ioctl(fd2, TIOCDISABLEECHO);
     dup2(fd2, STDIN_FILENO);
     disable_cursor();
+    clear_screen();
 
     for(i = 0; i < INITAL_FOOD_NUM; i++){
-        x = rand() % NUM_COLS;
-        y = rand() % NUM_ROWS;
-        food_pos = new_point(x, y);
+        food_pos = new_food();
         list_add(&food_pos->list, &board->foods);
+        draw_point(food_pos, FOOD_CHAR);
     }
     for(i = 1; i <= INITIAL_SNAKE_LEN; i++){
         snake_pos = new_point(i, 1);
         list_add(&snake_pos->list, &board->snake);
+        draw_point(snake_pos, SNAKE_CHAR);
         // fprintf(stderr, "add x %d y %d\n", i, 1);
     }
-    
-    // printf("fd %d\n", fd);
-    // exit(0);
 }
 
-void clear_screen(){
-    // byte stream for \e[2J
-    // static char cls[] = {0x1b, 0x5b, 0x31, 0x3b, 0x31, 0x48, 0x1b, 0x5b, 0x32, 0x4a, 0};
-    static char cls[] = {0x1b, 0x5b, 0x32, 0x4a, 0};
 
-    printf("%s", cls);
-}
 
 void debug_board(struct board_struct* board){
     struct point* p;
@@ -97,25 +130,6 @@ void debug_board(struct board_struct* board){
     fprintf(stderr, "\n");
 }
 
-void int2str(int value, int i, char* output){
-    int j;
-    while(i){
-        j = (value / i) % 10;
-        *output++ = '0' + j;
-        i /= 10;
-    }
-}
-
-void draw_point(struct point* pos, char character){
-    static char draw_pos[] = {0x1b, 0x5b, 0x30, 0x30, 0x30, 0x3b, 0x30, 0x30, 0x30, 0x48, 0};
-    static char format[] = "%s";
-    char *draw_format = draw_pos;
-    int2str(pos->y, 100, draw_format + 2);
-    int2str(pos->x, 100, draw_format + 6);
-    format[2] = character;
-    printf(format, draw_pos);
-    // fprintf(stderr, "x %d y %d; \n", pos->x, pos->y);
-}
 
 #define INPUT_SIZ   (12)
 
@@ -127,7 +141,7 @@ enum direction get_direction(struct board_struct* board, enum direction dir){
     int ret = read(board->ofd, input, INPUT_SIZ);
     if(ret > 0){
         char c = input[ret - 1];
-        // fprintf(stderr, "read %d ret %d ",ret, c);
+        // fprintf(stderr, "Direction %d \n",ret, c);
         switch (c)
         {
         case 'w':
@@ -154,10 +168,30 @@ enum direction get_direction(struct board_struct* board, enum direction dir){
     return dir;
 }
 
+struct point* get_snake_at_coordinate(struct board_struct* board, int x, int y){
+    struct point* p1, *p2;
+    list_for_each_entry_safe(struct point, p1, p2, &board->snake, list){
+        if(p1->x == x && p1->y == y){
+            return p1;
+        }
+    }
+    return NULL;
+}
+
+struct point* get_food_at_coordinate(struct board_struct* board, int x, int y){
+    struct point* p1, *p2;
+    list_for_each_entry_safe(struct point, p1, p2, &board->foods, list){
+        if(p1->x == x && p1->y == y){
+            return p1;
+        }
+    }
+    return NULL;
+}
+
 int refresh(struct board_struct* board, enum direction dir){
-    struct point* head = SNAKE_HEAD(board), *newpos, *pos;
+    struct point* head = GET_SNAKE_HEAD(board), *newpos, *pos;
     int x = head->x, y = head->y;
-    fprintf(stderr, "dir %d | ", dir);
+    // fprintf(stderr, "dir %d | ", dir);
     switch (dir)
     {
     case left:
@@ -186,32 +220,27 @@ int refresh(struct board_struct* board, enum direction dir){
         return FAIL;
     }
 
-    // pos = list_last_entry(&board->snake, struct point, list);
-    // list_del(&pos->list);
-    // delete_point(pos);
-    // newpos = new_point(x, y);
-    // list_add(&newpos->list, &board->snake);
+    pos = get_food_at_coordinate(board, x, y);
+    if(!pos){
+        pos = get_snake_at_coordinate(board, x, y);
+        if(pos){
+            return FAIL_EAT_SELF;
+        }
+        pos = list_last_entry(&board->snake, struct point, list);
+        clear_point(pos);
+    }else{
+        // if snake has eaten food, get a new random food
+        newpos = new_food();
+        list_add(&newpos->list, &board->foods);
+        draw_point(newpos, FOOD_CHAR);
+    }
 
-    pos = list_last_entry(&board->snake, struct point, list);
     list_del(&pos->list);
     pos->x = x;
     pos->y = y;
     list_add(&pos->list, &board->snake);
-    // debug_board(board);
+    draw_point(pos, SNAKE_CHAR);
 
-    // pos = list_last_entry(&board->snake, struct point, list);
-    // pos->x = x;
-    // pos->y = y;
-
-    // x++;
-    // if(x > NUM_COLS){
-    //     x = 1;
-    //     y++;
-    // }
-    // if(y > NUM_ROWS){
-    //     y = 1;
-    //     x = 1;
-    // }
     return SUCCESS;
 }
 
@@ -236,15 +265,13 @@ int main(int argc, char** argv){
     enum direction dir = right;
     board_init(bp);
     while(1){
-        int tmp = 10000;
-        clear_screen();
+        int tmp = 20000;
         dir = get_direction(bp, dir);
         ret = refresh(bp, dir);
         if(ret){
             printf("failed %d\n", ret);
             break;
         }
-        draw(bp);
         while(tmp--);
     }
     return 0;
