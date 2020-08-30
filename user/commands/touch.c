c65b668d3f41e66bc11c9edbeb8c9ba3fd90f28e
Bruce Tan <brucetansh@gmail.com>
2020-08-30 19:28:22 +1200

    Touch command implemented

---
+++ user/commands/touch.c
@@ -0,0 +1,15 @@
+#include <sys/syscall.h>
+#include <sys/fcntl.h>
+
+int main(int argc, char *argv[]){
+    int fd;
+    if(argc < 2 ){
+        fprintf(stderr, "mising file operand\n");
+        return 1;
+    }
+    fd = open(argv[1], O_CREAT | O_RDWR, 0x755 );
+    close(fd);
+    return 0;
+}
+
+
