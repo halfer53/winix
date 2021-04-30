d154a1aa823dd242a7b697ba2c9c4ae0cb458bfd
Bruce Tan <brucetansh@gmail.com>
2021-05-01 01:42:10 +1200

    report error if path is directory in cat(3)

--- user/commands/cat.c
+++ user/commands/cat.c
@@ -1,16 +1,27 @@
 #include <sys/syscall.h>
 #include <sys/fcntl.h>
-
+#include <sys/stat.h>
 
 int main(int argc, char *argv[]){
     int fd, ret;
     char buf[256]; 
+    struct stat statbuf;
+    char *file = argv[1];
 
     if(argc < 2){
         // fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
         fd = STDIN_FILENO;
     }else{
-        fd = open(argv[1], O_RDONLY);
+        ret = stat(file, &statbuf);
+        if(ret){
+            perror("open");
+            return 1;
+        }
+        if(S_ISDIR(statbuf.st_mode)){
+            fprintf(stderr, "%s is a directory\n", file);
+            return 1;
+        }
+        fd = open(file, O_RDONLY);
         if(fd < 0){
             perror("open");
             return 1;
