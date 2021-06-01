f64198d83604b0cba1d8dd97b4a16dcc81d44543
Bruce Tan <brucetansh@gmail.com>
2021-06-02 09:52:46 +1200

    remove size_t_to_binary

--- include/fs_include/util.h
+++ include/fs_include/util.h
@@ -1,7 +1,6 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_ 1
 
-const char *size_t_to_binary(unsigned int x);
 int hex2int(char *a, int len);
 
 #endif
--- lib/util/util.c
+++ lib/util/util.c
@@ -1,18 +1,5 @@
 #include <util.h>
 
-const char *size_t_to_binary(unsigned int x)
-{
-    static char b[32];
-    int z;
-    b[0] = '\0';
-
-    for (z = 128; z > 0; z >>= 1)
-    {
-        strcat(b, ((x & z) == z) ? "1" : "0");
-    }
-
-    return b;
-}
 
 int hex2int(char *a, int len)
 {
