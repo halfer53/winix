bfd2f72da7d4e5eeccdc2a9e9ea1bfa8185061a7
Bruce Tan <brucetansh@gmail.com>
2021-06-05 13:16:22 +1200

    change block size to 512 bytes

--- include/fs/common.h
+++ include/fs/common.h
@@ -7,16 +7,16 @@
 
 #define ROOT_INODE_NUM  1
 
-#define BLOCK_SIZE_WORD  (1024)
+#define BLOCK_SIZE_WORD  (128)
 
 #ifdef __x86_64__
 #define SECTOR_SIZE 512
-#define BLOCK_SIZE  4096
+#define BLOCK_SIZE  512
 #define TO_WORD_SIZE(x) (x / 4)
 #define TO_CHAR_SIZE(x) (x * 4)
 #else
 #define SECTOR_SIZE 128
-#define BLOCK_SIZE  1024
+#define BLOCK_SIZE  128
 #define TO_WORD_SIZE(x) (x)
 #define TO_CHAR_SIZE(x) (x)
 #endif
@@ -42,7 +42,7 @@
 
 #define SUPER_BLOCK_MAGIC   (0xabcdefff)
 
-#define DISK_PAGE_NR    (50)
+#define DISK_PAGE_NR    (400)
 #define DISK_SIZE   (DISK_PAGE_NR * BLOCK_SIZE)
 #define DISK_SIZE_WORD   (DISK_PAGE_NR * BLOCK_SIZE_WORD)
 
