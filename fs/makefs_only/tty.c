755600b4e7d5c085def1a1583afc48f5647a9fc1
Bruce Tan <brucetansh@gmail.com>
2021-10-19 23:02:55 +1300

    Fix warning

--- fs/makefs_only/tty.c
+++ fs/makefs_only/tty.c
@@ -8,16 +8,16 @@
 struct device tty_dev_static;
 static struct filp_operations fops;
 static struct device_operations dops;
-static char* name = "tty";
+static const char* name = "tty";
 
 int tty_read ( struct filp *filp, char *data, size_t count, off_t offset){
     memset(data, 't', count);
     data[count - 1] = '\0';
-    return count;
+    return *(int *)&count;
 }
 
 int tty_write ( struct filp *filp, char *data, size_t count, off_t offset){
-    return count;
+    return *(int *)&count;
 }
 
 int tty_open ( struct device* dev, struct filp *file){
