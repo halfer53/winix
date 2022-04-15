762d1b54cc90b3a527e47397b604ee86f66db5f9
Bruce Tan <brucetansh@gmail.com>
2022-04-15 17:16:57 +1200

    rename sys_fcntl

--- fs/system/fcntl.c
+++ fs/system/fcntl.c
@@ -4,7 +4,7 @@
 
 #include "../fs.h"
 
-int __sys_fcntl(struct proc* who, int fd, int cmd, void* arg){
+int sys_fcntl(struct proc* who, int fd, int cmd, void* arg){
     struct filp* file;
     if(!is_fd_opened_and_valid(who, fd))
         return EBADF;
@@ -26,7 +26,7 @@ int __sys_fcntl(struct proc* who, int fd, int cmd, void* arg){
 int do_fcntl(struct proc* who, struct message* msg){
     if(!is_vaddr_accessible(msg->m1_p1, who))
         return EFAULT;
-    return __sys_fcntl(who, msg->m1_i1, msg->m1_i2, msg->m1_p1);
+    return sys_fcntl(who, msg->m1_i1, msg->m1_i2, msg->m1_p1);
 }
 
 
