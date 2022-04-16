df1fb710dec75b6cbc45afe3a8033b0ff1f6e4b1
Bruce Tan <brucetansh@gmail.com>
2022-04-17 02:51:05 +1200

    minor readability improvement

--- fs/system/fcntl.c
+++ fs/system/fcntl.c
@@ -14,7 +14,7 @@ int sys_fcntl(struct proc* who, int fd, int cmd, void* arg){
     case F_GETFL:
         return file->filp_flags;
     case F_SETFL:
-        file->filp_flags = *((int*)arg + (int)((char *)who->ctx.rbase - (char *)0)); 
+        file->filp_flags = *((int *)arg); 
         break;
 
     default:
@@ -26,7 +26,7 @@ int sys_fcntl(struct proc* who, int fd, int cmd, void* arg){
 int do_fcntl(struct proc* who, struct message* msg){
     if(!is_vaddr_accessible(msg->m1_p1, who))
         return EFAULT;
-    return sys_fcntl(who, msg->m1_i1, msg->m1_i2, msg->m1_p1);
+    return sys_fcntl(who, msg->m1_i1, msg->m1_i2, get_physical_addr(msg->m1_p1, who));
 }
 
 
