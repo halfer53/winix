73c7235e0d29068acefac0eb378a679c72f29452
Bruce Tan <brucetansh@gmail.com>
2022-04-17 02:53:26 +1200

    Revert "Delete redundant code"
    This reverts commit 55d86a2387336086a02603d6afa7980b7bd70321.

--- fs/system/read_write.c
+++ fs/system/read_write.c
@@ -3,6 +3,7 @@
 
 int sys_read(struct proc *who,int fd, void *buf, size_t count){
     struct filp* file;
+    SET_CALLER(who);
     if(!is_fd_opened_and_valid(who, fd))
         return EBADF;
     file = who->fp_filp[fd];
@@ -13,6 +14,7 @@ int sys_read(struct proc *who,int fd, void *buf, size_t count){
 int sys_write(struct proc *who,int fd, void *buf, size_t count){
     struct filp* file;
     int ret;
+    SET_CALLER(who);
     if(!is_fd_opened_and_valid(who, fd))
         return EBADF;
     file = who->fp_filp[fd];
