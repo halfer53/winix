c43142ad383d7ac1a4bd51f613638af31dc695f8
Bruce Tan <brucetansh@gmail.com>
2022-04-18 21:31:34 +1200

    Add error checking in setsid

--- kernel/system/do_setsid.c
+++ kernel/system/do_setsid.c
@@ -2,6 +2,9 @@
 
 int do_setsid(struct proc* who, struct message* m){
     pid_t pid = who->pid;
+    if (who->procgrp == pid){
+        return EPERM;
+    }
     who->session_id = pid;
     who->procgrp = pid;
     return pid;
