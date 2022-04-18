268ecf912784707012be48d532da68f28986f197
Bruce Tan <brucetansh@gmail.com>
2022-04-18 16:24:27 +1200

    Add tests for parser

--- tests/parse_test.c
+++ tests/parse_test.c
@@ -4,9 +4,20 @@
 #include <assert.h>
 #include <string.h>
 
-void test_given_cmdline_when_4_pipes_should_return_4_argc(){
+void test_given_cmdline_when_4_pipes_should_return_4(){
     struct cmdLine cmd;
     int ret = parse("ls | grep | wc | cat", &cmd);
+    assert(ret == 0);
+    assert(cmd.numCommands == 4);
+    assert(strcmp(cmd.argv[cmd.cmdStart[0]], "ls") == 0);
+    assert(strcmp(cmd.argv[cmd.cmdStart[1]], "grep") == 0);
+    assert(strcmp(cmd.argv[cmd.cmdStart[2]], "wc") == 0);
+    assert(strcmp(cmd.argv[cmd.cmdStart[3]], "cat") == 0);
+}
+
+void test_given_cmdline_when_args_4_pipes_should_return_4(){
+    struct cmdLine cmd;
+    int ret = parse("ls -lah bin | grep snake | wc | cat", &cmd);
     assert(ret == 0);
     assert(cmd.numCommands == 4);
     assert(strcmp(cmd.argv[cmd.cmdStart[0]], "ls") == 0);
