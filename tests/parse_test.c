ea91d74141326701d053ee192a111e1625128782
Bruce Tan <brucetansh@gmail.com>
2022-04-18 16:27:45 +1200

    Add tests to parser

--- tests/parse_test.c
+++ tests/parse_test.c
@@ -25,3 +25,15 @@ void test_given_cmdline_when_args_4_pipes_should_return_4(){
     assert(strcmp(cmd.argv[cmd.cmdStart[2]], "wc") == 0);
     assert(strcmp(cmd.argv[cmd.cmdStart[3]], "cat") == 0);
 }
+
+void test_given_cmdline_when_args_5_pipes_should_return_5(){
+    struct cmdLine cmd;
+    int ret = parse("ls -lah bin | grep snake | wc -l | cat | cat", &cmd);
+    assert(ret == 0);
+    assert(cmd.numCommands == 5);
+    assert(strcmp(cmd.argv[cmd.cmdStart[0]], "ls") == 0);
+    assert(strcmp(cmd.argv[cmd.cmdStart[1]], "grep") == 0);
+    assert(strcmp(cmd.argv[cmd.cmdStart[2]], "wc") == 0);
+    assert(strcmp(cmd.argv[cmd.cmdStart[3]], "cat") == 0);
+    assert(strcmp(cmd.argv[cmd.cmdStart[4]], "cat") == 0);
+}
