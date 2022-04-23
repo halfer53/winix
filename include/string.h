04d3574d0668b4ca14fc8ca8ef39443f8c2f74e0
Bruce Tan <brucetansh@gmail.com>
2022-04-23 19:42:39 +1200

    Add definition of memcmp

--- include/string.h
+++ include/string.h
@@ -28,6 +28,7 @@ size_t strcspn(const char *s1, const char *s2);
 size_t strspn(const char *s1, const char *s2);
 void *memmove(void *dest, const void *src, size_t n);
 char *strrchr(const char *str, int c);
+int memcmp(const void *str1, const void *str2, size_t n);
 
 #define strchr(s, c)    index(s, c)
 
