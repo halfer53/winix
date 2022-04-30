336e13cc260c3c0f6652f1c3edb45846796d9c04
Bruce Tan <brucetansh@gmail.com>
2022-04-30 18:43:22 +1200

    remove comments

--- lib/syscall/printf.c
+++ lib/syscall/printf.c
@@ -3,21 +3,12 @@
 #include <stddef.h>
 #include <stdio.h>
 
-
 FILE _stdin =  {0, 0, _IODEFAULT, 0 , 0, 0};
 FILE _stdout = {0, 1, _IODEFAULT, 0 , 0, 0};
 FILE _stderr = {0, 2, _IODEFAULT, 0 , 0, 0};
 FILE *stdin = &_stdin, *stdout = &_stdout, *stderr = &_stderr;
 
 char estr[ESTR_SIZ];
-
-// int __dprintf(int fd, const char *format, void* args){
-//     struct message m;
-//     m.m1_i1 = fd;
-//     m.m1_p1 = (void *)format;
-//     m.m1_p2 = args;
-//     return _syscall(DPRINTF, &m);
-// }
 
 int dprintf(int fd, const char *format, ...){
     
@@ -31,14 +22,6 @@ int fprintf(FILE *stream, const char *format, ...){
 int printf(const char *format, ...) {
     return __dprintf(1, format, (int*)&format + 1);
 }
-
-// int __strerror(char *buffer, int len){
-//     struct message m;
-//     m.m1_p1 = buffer;
-//     m.m1_i1 = len;
-//     m.m1_i2 = errno;
-//     return _syscall(STRERROR, &m);
-// }
 
 char* strerror(int usrerrno){
     __strerror(estr, ESTR_SIZ, usrerrno);
