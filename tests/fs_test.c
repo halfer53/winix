33335574f888de7df18822c683822f6822e0acb2
Bruce Tan <brucetansh@gmail.com>
2022-05-01 17:35:12 +1200

    add zone iter test

--- tests/fs_test.c
+++ tests/fs_test.c
@@ -585,3 +585,13 @@ void test_when_zone_full_should_return_enospc(){
     ret = sys_write(current, fd, _buffer, 1);
     assert(ret == -EFBIG);
 }
+
+void test_given_zone_iterator_should_return(){
+    struct zone_iterator iter;
+    struct device* dev = get_dev(ROOT_DEV);
+    struct inode* root = get_inode(ROOT_INODE_NUM, dev);
+    int ret = init_zone_iterator(&iter, root, 0);
+    assert(ret == 0);
+    assert(iter.i_inode == root);
+    assert(iter.i_zone_idx == 0);
+}
