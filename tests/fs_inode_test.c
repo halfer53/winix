f9b4a570ba7e439a33ee828f65ac06ecee062e4c
Bruce Tan <brucetansh@gmail.com>
2022-05-01 18:06:07 +1200

    Add tests

--- tests/fs_inode_test.c
+++ tests/fs_inode_test.c
@@ -10,3 +10,14 @@ void test_given_zone_iterator_should_return(){
     assert(iter.i_inode == root);
     assert(iter.i_zone_idx == 0);
 }
+
+void test_given_has_next_zone_when_have_zone_should_return_true(){
+    struct zone_iterator iter;
+    struct device* dev = get_dev(ROOT_DEV);
+    struct inode* root = get_inode(ROOT_INODE_NUM, dev);
+    int ret = init_zone_iterator(&iter, root, 0);
+    assert(ret == 0);
+    
+    bool result = has_next_zone(&iter);
+    assert(result == true);
+}
