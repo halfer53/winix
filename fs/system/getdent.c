270a2f0f1497ccd4e67848c3140b110cad8b19b5
Bruce Tan <brucetansh@gmail.com>
2022-05-02 12:30:40 +1200

    refactor sys_getdents

--- fs/system/getdent.c
+++ fs/system/getdent.c
@@ -5,12 +5,11 @@
 #include <fs/fs.h>
 
 int sys_getdents(struct proc* who, int fd, struct dirent* dirp_dst, unsigned int count){
-    int i, dirstream_nr, ret = 0;
-    struct block_buffer *buffer;
-    struct winix_dirent* dirstream, *endstream;
+    struct dirent_iterator iter;
     struct filp* file;
     struct inode* dirp;
-    block_t bnr;
+    struct winix_dirent* dirstream;
+    int ret = 0;
 
     if(!is_fd_opened_and_valid(who, fd))
         return -EBADF;
@@ -21,44 +20,29 @@ int sys_getdents(struct proc* who, int fd, struct dirent* dirp_dst, unsigned int
     if(!S_ISDIR(dirp->i_mode))
         return -ENOTDIR;
 
-    dirstream_nr = file->getdents_dirstream_nr;
-    i = file->getdents_zone_nr;
-    for(; i < NR_TZONES; i++){
-        bnr = dirp->i_zone[i];
-        if(bnr > 0){
-            // KDEBUG(("getting block %d for inode %d\n", bnr, dirp->i_num));
-            buffer = get_block_buffer(bnr, dirp->i_dev);
-            endstream = (struct winix_dirent*)&buffer->block[BLOCK_SIZE];
-            dirstream = (struct winix_dirent*)buffer->block;
-            dirstream += dirstream_nr;
-
-            while(dirstream < endstream){
-                if(!count)
-                    break;
-
-                if(dirstream->dirent.d_ino > 0) {
-                    memcpy(dirp_dst++, &dirstream->dirent, sizeof(struct dirent));
-                    ret += sizeof(struct dirent);
-                    file->getdents_dirstream_nr = dirstream_nr + 1;
-                    count--;
-                }
-                dirstream++;
-                dirstream_nr++;
-            }
-            if(count){
-                file->getdents_zone_nr++;
-                dirstream_nr = 0;
-            }
-            put_block_buffer(buffer);
+    iter_dirent_init(&iter, dirp, file->getdents_zone_nr, file->getdents_dirstream_nr);
+    while(iter_dirent_has_next(&iter)){
+        if(!count)
+            break;
+        
+        dirstream = iter_dirent_get_next(&iter);
+        if(dirstream->dirent.d_ino > 0) {
+            memcpy(dirp_dst++, &dirstream->dirent, sizeof(struct dirent));
+            ret += sizeof(struct dirent);
+            count--;
         }
     }
+    file->getdents_zone_nr = iter.zone_iter.i_zone_idx - 1;
+    file->getdents_dirstream_nr = iter.dirent - (struct winix_dirent*)iter.buffer->block;
+    iter_dirent_close(&iter);
     return ret;
 }
 
 int do_getdents(struct proc* who, struct message* msg){
     struct dirent* path = (struct dirent *) get_physical_addr(msg->m1_p1, who);
-    if(!is_vaddr_accessible(msg->m1_p1, who))
+    int count = msg->m1_i2;
+    if(!is_vaddr_ok(who, msg->m1_p1, sizeof(struct dirent) * count))
         return -EFAULT;
-    return sys_getdents(who, msg->m1_i1, path, msg->m1_i2);
+    return sys_getdents(who, msg->m1_i1, path, count);
 }
 
