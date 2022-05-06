0a82b10e92a267e62335fc8ecec31129bfdfc7cf
Bruce Tan <brucetansh@gmail.com>
2022-05-06 13:04:17 +1200

    implement fcntl in variadic macro

--- kernel/system.c
+++ kernel/system.c
@@ -198,17 +198,6 @@ void set_syscall_mesg_exception(int operation, ptr_t* osp, struct message *m, st
         m->m1_p1 = (void *)*sp++;
         m->m1_p2 = (void *)*sp;
         break;
-
-    // This is bit hacky, since C89 does not support variadic macro, so we have to 
-    // implement open(2) ioctl(2) and fcntl(2) in assembly to support direct syscall mode
-    // who->ctx.m.sp++ basically clears the first parameter which is the system call number
-    // set by the assembly function in lib/syscall/variadic_syscall.s
-    // case OPEN:
-    //     who->ctx.m.sp++;
-    //     m->m1_p1 = (void *)*sp++;
-    //     m->m1_i1 = *sp++;
-    //     m->m1_i2 = *sp++;
-    //     break;
     
     case FCNTL:
     case IOCTL:
