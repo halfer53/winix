e53750ac7c18eee0588c1341d82b28010df78dc1
Bruce Tan <brucetansh@gmail.com>
2022-05-07 00:09:40 +1200

    move strcspn

---
+++ lib/ansi/strcspn.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include <string.h>
+
+size_t strcspn(const char *s1, const char *s2)
+{
+    size_t ret=0;
+    while(*s1)
+        if(strchr(s2,*s1))
+            return ret;
+        else
+            s1++,ret++;
+    return ret;
+}
--- lib/ansi/string.c
+++ lib/ansi/string.c
@@ -5,23 +5,6 @@
 
 
 
-
-
-
-
-
-
-size_t strcspn(const char *s1, const char *s2)
-{
-    size_t ret=0;
-    while(*s1)
-        if(strchr(s2,*s1))
-            return ret;
-        else
-            s1++,ret++;
-    return ret;
-}
-
 size_t strspn(const char *s1, const char *s2)
 {
     size_t ret=0;
