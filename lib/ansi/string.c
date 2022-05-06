0f498c6314f9d2f2e3ccbd3d376040f063843c9c
Bruce Tan <brucetansh@gmail.com>
2022-05-07 00:10:39 +1200

    move strspn

--- lib/ansi/string.c
+++ lib/ansi/string.c
@@ -5,13 +5,7 @@
 
 
 
-size_t strspn(const char *s1, const char *s2)
-{
-    size_t ret=0;
-    while(*s1 && strchr(s2,*s1++))
-        ret++;
-    return ret;    
-}
+
 
 void *memmove(void *dest, const void *src, size_t n)
 {
---
+++ lib/ansi/strspn.c
@@ -0,0 +1,9 @@
+#include <string.h>
+
+size_t strspn(const char *s1, const char *s2)
+{
+    size_t ret=0;
+    while(*s1 && strchr(s2,*s1++))
+        ret++;
+    return ret;    
+}
