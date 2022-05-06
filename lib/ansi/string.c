25bfe65de5b115d139e50695352aa9f471ad0b88
Bruce Tan <brucetansh@gmail.com>
2022-05-06 23:58:41 +1200

    move ststr

--- lib/ansi/string.c
+++ lib/ansi/string.c
@@ -7,35 +7,9 @@
 
 
 
-// returns true if X and Y are same
-int compare(const char *X, const char *Y)
-{
-	while (*X && *Y)
-	{
-		if (*X != *Y)
-			return 0;
 
-		X++;
-		Y++;
-	}
 
-	return (*Y == '\0');
-}
 
-// Function to implement strstr() function
-char* strstr(const char* haystack, const char* needle)
-{
-    const char *X = haystack;
-    const char *Y = needle;
-	while (*X != '\0')
-	{
-		if ((*X == *Y) && compare(X, Y))
-			return (char *)(unsigned long)X;
-		X++;
-	}
-
-	return NULL;
-}
 
 size_t strcspn(const char *s1, const char *s2)
 {
