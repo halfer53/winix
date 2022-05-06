6997b660b4865a1403e183f4ab5a3131d5a6aad8
Bruce Tan <brucetansh@gmail.com>
2022-05-07 00:49:52 +1200

    use std macro

--- lib/stdio/dprintf.c
+++ lib/stdio/dprintf.c
@@ -2,9 +2,9 @@
 #include <stddef.h>
 #include <stdio.h>
 
-FILE _stdin =  {0, 0, _IODEFAULT, 0 , 0, 0};
-FILE _stdout = {0, 1, _IODEFAULT, 0 , 0, 0};
-FILE _stderr = {0, 2, _IODEFAULT, 0 , 0, 0};
+FILE _stdin =  {0, STDIN_FILENO, _IODEFAULT, 0 , 0, 0};
+FILE _stdout = {0, STDOUT_FILENO, _IODEFAULT, 0 , 0, 0};
+FILE _stderr = {0, STDERR_FILENO, _IODEFAULT, 0 , 0, 0};
 
 int dprintf(int fd, const char *format, ...){
     
