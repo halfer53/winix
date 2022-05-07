21ce89a4b3a2f609c659a3ca969309fc2016daf1
Bruce Tan <brucetansh@gmail.com>
2022-05-07 23:50:33 +1200

    restructure header file

--- include_winix/sys/types.h
+++ include_winix/sys/types.h
@@ -1,9 +1,25 @@
 #ifndef _TYPE_H_
 #define _TYPE_H_ 1
 
-#include <sys/stat.h>
 #include <stdint.h>
-typedef int pid_t;
 
+typedef int pid_t;
+typedef unsigned int mode_t;
+typedef unsigned int uid_t;
+typedef unsigned int gid_t;
+typedef unsigned int nlink_t;
+typedef unsigned int blksize_t;
+typedef unsigned int blkcnt_t;
+typedef unsigned int zone_t;
+typedef int ino_t;
+typedef unsigned int block_t;
+typedef unsigned int sector_t;
+typedef unsigned int dev_t;       /* holds (major|minor) device pair */
+typedef unsigned int bitchunk_t; /* collection of bits in a bitmap */
+
+# ifndef __off_t_defined
+typedef unsigned int off_t;
+# define __off_t_defined
+#endif
 
 #endif
