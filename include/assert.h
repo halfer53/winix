887181d73b3504c424974c6c309cc1d443077c7f
Bruce Tan <brucetansh@gmail.com>
2022-05-07 19:55:33 +1200

    update assert.h

--- include/assert.h
+++ include/assert.h
@@ -1,8 +1,8 @@
 #ifndef _ASSERT_H_
 #define _ASSERT_H_
 
-void _assert(int expression, int line, char* filename);
+void __assert(int expression, int line, char* filename);
 
-#define assert(expression)    _assert(expression, __LINE__ , __FILE__)
+#define assert(expression)    __assert(expression, __LINE__ , __FILE__)
 
 #endif
