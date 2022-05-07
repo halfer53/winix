d8a76e5e1af175fa53efe4e0a1a7f4d73e466bd1
Bruce Tan <brucetansh@gmail.com>
2022-05-07 14:57:06 +1200

    echo to print multip args

--- user/commands/echo.c
+++ user/commands/echo.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-static char buffer[128];
+static char buffer[512];
 
 int main(int argc, char *argv[]){
     char *p, *dest;
     char c;
+    int i = 1;
     if(argc < 2)
         return 1;
-    p = argv[1];
     dest = buffer;
-    while(*p){
-        c = *p;
-        if(c == '\\'){
-            c = *++p;
-            switch (c)
-            {
-            case 'n':
-                c = '\n';
-                break;
-            case 't':
-                c = '\t';
-                break;
-            
-            default:
-                break;
+    while (i < argc){
+        p = argv[i];
+        if (i > 1)
+            *dest++ = ' ';
+        while(*p){
+            c = *p;
+            if(c == '\\'){
+                c = *++p;
+                switch (c)
+                {
+                case 'n':
+                    c = '\n';
+                    break;
+                case 't':
+                    c = '\t';
+                    break;
+                
+                default:
+                    break;
+                }
+                
             }
-            
+            *dest++ = c;
+            p++;
         }
-        *dest++ = c;
-        p++;
+        *dest = '\0';
+        i++;
     }
-    *dest = '\0';
+    *dest++ = '\n';
     printf("%s", buffer);
     return 0;
 }
