50760d1f89b51f7f35e0ca24b91ab87f5eedd419
Bruce Tan <brucetansh@gmail.com>
2022-05-08 18:16:55 +1200

    init environ in method get_environ

--- lib/ansi/env.c
+++ lib/ansi/env.c
@@ -1,3 +1,21 @@
 #include <stddef.h>
+#include <sys/debug.h>
+#include <stdio.h>
 
 const char** _environ = NULL;
+/**
+ * @brief pointer to environment variable is stored at the bottom of stack by convention
+ * refer to build_user_stack in kernel/system/do_execve.c
+ * 
+ */
+
+#define __ALIGN1K(x) 	    (((((x))>>10)<<10)+1023)
+#define __get_env_address() (__ALIGN1K((unsigned long)get_sp()))
+#define __get_env_ptr()     (*((const char ***) (unsigned long)__get_env_address()))
+
+const char** get_environ(){
+    if(!_environ){
+        _environ = __get_env_ptr();
+    }
+    return _environ;
+}
