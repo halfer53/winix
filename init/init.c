fb5c121e8b92f241c7a23053203633e009600c0a
Bruce Tan <brucetansh@gmail.com>
2022-05-08 18:42:43 +1200

    use assert in init

--- init/init.c
+++ init/init.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <assert.h>
 
 char shell_path[] = "/bin/bash";
 char *shell_argv[] = {
@@ -16,18 +17,6 @@ char *shell_argv[] = {
 };
 
 int pgid;
-
-#define CHECK_SYSCALL(cond)    \
-    if (!(cond))               \
-    {                          \
-        failed_init(__LINE__); \
-    }
-
-void failed_init(int line)
-{
-    printf("init %d failed at %d, err %s\n", getpid(), line, strerror(errno));
-    exit(1);
-}
 
 void block_signals()
 {
@@ -47,34 +36,34 @@ void init_dev()
     int ret;
 
     ret = mkdir("/dev", 0x755);
-    CHECK_SYSCALL(ret == 0);
+    assert(ret == 0);
 
     ret = mknod("/dev/tty1", 0x755, TTY1_DEV_NUM);
-    CHECK_SYSCALL(ret == 0);
+    assert(ret == 0);
 
     ret = mknod("/dev/tty2", 0x755, TTY2_DEV_NUM);
-    CHECK_SYSCALL(ret == 0);
+    assert(ret == 0);
 
     ret = mknod("/dev/sda", 0x755, ROOT_DEV);
-    CHECK_SYSCALL(ret == 0);
+    assert(ret == 0);
 }
 
 void init_tty()
 {
     int fd, ret;
     ret = setsid();
-    CHECK_SYSCALL(ret == getpid());
+    assert(ret == getpid());
     fd = open("/dev/tty1", O_RDONLY);
-    CHECK_SYSCALL(fd == 0);
+    assert(fd == 0);
     ret = dup(fd);
-    CHECK_SYSCALL(ret == 1);
+    assert(ret == 1);
     ret = dup(fd);
-    CHECK_SYSCALL(ret == 2);
+    assert(ret == 2);
     ret = ioctl(STDIN_FILENO, TIOCSCTTY, 0);
-    CHECK_SYSCALL(ret == 0);
+    assert(ret == 0);
     pgid = getpgid(0);
     ret = ioctl(STDIN_FILENO, TIOCSPGRP, &pgid);
-    CHECK_SYSCALL(ret == 0);
+    assert(ret == 0);
 }
 
 void start_init_routine()
@@ -106,7 +95,7 @@ void run_unit_test()
         _exit(1);
     }
     wait(&status);
-    CHECK_SYSCALL(WIFEXITED(status));
+    assert(WEXITSTATUS(status) == 0);
 }
 
 void run_shell(){
