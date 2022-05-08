4f808c64d966638a78ca6cdbf5d34a190df8c214
Bruce Tan <brucetansh@gmail.com>
2022-05-08 18:16:22 +1200

    update environ macro

--- include/stdlib.h
+++ include/stdlib.h
@@ -6,12 +6,9 @@
 #include <sys/syscall.h>
 
 extern const char **_environ;
+const char** get_environ();
 
-#define __ALIGN1K(x) 	    (((((x))>>10)<<10)+1023)
-#define __get_env_address() (__ALIGN1K((unsigned long)get_sp()))
-#define __get_env_ptr()     (*((const char ***) (unsigned long)__get_env_address()))
-#define __get_env()         (_environ ? _environ : __get_env_ptr()) 
-#define init_environ()      (_environ = __get_env())
+#define environ get_environ()
 
 #ifndef NULL
 #define	NULL		((void *)0)
