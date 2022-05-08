7389d18335fcdd8151c0856a7b57b0e8c1a3235b
Bruce Tan <brucetansh@gmail.com>
2022-05-08 17:06:56 +1200

    run unit test in bash

--- init/init.c
+++ init/init.c
@@ -17,120 +17,109 @@ char *shell_argv[] = {
 
 int pgid;
 
-#define CHECK_SYSCALL(cond) \
-  if (!(cond))              \
-  {                         \
-    failed_init(__LINE__);  \
-  }
+#define CHECK_SYSCALL(cond)    \
+    if (!(cond))               \
+    {                          \
+        failed_init(__LINE__); \
+    }
 
 void failed_init(int line)
 {
-  printf("init %d failed at %d, err %s\n", getpid(), line, strerror(errno));
-  exit(1);
+    printf("init %d failed at %d, err %s\n", getpid(), line, strerror(errno));
+    exit(1);
 }
 
 void block_signals()
 {
-  int i;
-  sigset_t mask;
-  sigfillset(&mask);
-  sigdelset(&mask, SIGSEGV);
-  sigprocmask(SIG_SETMASK, &mask, NULL);
-  for(i = 1; i < _NSIG; i++){
-    signal(i, SIG_IGN);
-  }
-}
-
-void init_dev(){
-  int ret;
-
-  ret = mkdir("/dev", 0x755);
-  CHECK_SYSCALL(ret == 0);
-
-  ret = mknod("/dev/tty1", 0x755, TTY1_DEV_NUM);
-  CHECK_SYSCALL(ret == 0);
-
-  ret = mknod("/dev/tty2", 0x755, TTY2_DEV_NUM);
-  CHECK_SYSCALL(ret == 0);
-
-  ret = mknod("/dev/sda", 0x755, ROOT_DEV);
-  CHECK_SYSCALL(ret == 0);
-
-}
-
-void init_tty(){
-  int fd, ret;
-  ret = setsid();
-  CHECK_SYSCALL(ret == getpid());
-  fd = open("/dev/tty1", O_RDONLY);
-  CHECK_SYSCALL(fd == 0);
-  ret = dup(fd);
-  CHECK_SYSCALL(ret == 1);
-  ret = dup(fd);
-  CHECK_SYSCALL(ret == 2);
-  ret = ioctl(STDIN_FILENO, TIOCSCTTY, 0);
-  CHECK_SYSCALL(ret == 0);
-  pgid = getpgid(0);
-  ret = ioctl(STDIN_FILENO, TIOCSPGRP, &pgid);
-  CHECK_SYSCALL(ret == 0);
-}
-
-void write_dummy_file(){
-  int fd, ret, i, j;
-  char c = 'a';
-  char buffer[128];
-  char *p = buffer;
-
-  fd = open("/foo", O_CREAT | O_RDWR, 0x755);
-  CHECK_SYSCALL(fd > 0);
-  for (i = 0; i < 6; i++)
-  {
-    for (j = 0; j < 4; j++)
+    int i;
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask, SIGSEGV);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+    for (i = 1; i < _NSIG; i++)
     {
-      *p++ = c++;
+        signal(i, SIG_IGN);
     }
-    *p = '\0';
-    dprintf(fd, "%s\n", buffer);
-    p = buffer;
-  }
-  ret = close(fd);
-  CHECK_SYSCALL(ret == 0);
 }
 
-void start_init_routine(){
-  pid_t pid;
-  int status;
-  block_signals();
-  while (1)
-  {
-    pid = wait(&status);
-    if (pid == -1)
-    {
-      break;
-    }
-    // printf("INIT: child %d exit status %d\n", pid, WEXITSTATUS(status));
-  }
+void init_dev()
+{
+    int ret;
 
-  printf("Shutting down...\n");
+    ret = mkdir("/dev", 0x755);
+    CHECK_SYSCALL(ret == 0);
+
+    ret = mknod("/dev/tty1", 0x755, TTY1_DEV_NUM);
+    CHECK_SYSCALL(ret == 0);
+
+    ret = mknod("/dev/tty2", 0x755, TTY2_DEV_NUM);
+    CHECK_SYSCALL(ret == 0);
+
+    ret = mknod("/dev/sda", 0x755, ROOT_DEV);
+    CHECK_SYSCALL(ret == 0);
+}
+
+void init_tty()
+{
+    int fd, ret;
+    ret = setsid();
+    CHECK_SYSCALL(ret == getpid());
+    fd = open("/dev/tty1", O_RDONLY);
+    CHECK_SYSCALL(fd == 0);
+    ret = dup(fd);
+    CHECK_SYSCALL(ret == 1);
+    ret = dup(fd);
+    CHECK_SYSCALL(ret == 2);
+    ret = ioctl(STDIN_FILENO, TIOCSCTTY, 0);
+    CHECK_SYSCALL(ret == 0);
+    pgid = getpgid(0);
+    ret = ioctl(STDIN_FILENO, TIOCSPGRP, &pgid);
+    CHECK_SYSCALL(ret == 0);
+}
+
+void start_init_routine()
+{
+    pid_t pid;
+    int status;
+    block_signals();
+    while (1)
+    {
+        pid = wait(&status);
+        if (pid == -1)
+        {
+            break;
+        }
+        // printf("INIT: child %d exit status %d\n", pid, WEXITSTATUS(status));
+    }
+
+    printf("Shutting down...\n");
+}
+
+void run_unit_test()
+{
+    char *argv[] = {shell_path, "-c", "test", "run", ">", "test.log", NULL};
+    if (!vfork()){
+        execv(shell_path, argv);
+        _exit(1);
+    }
+}
+
+void run_shell(){
+    if (!vfork())
+    {
+        execv(shell_path, shell_argv);
+        _exit(1);
+    }
 }
 
 int main(int argc, char **argv)
 {
-  pid_t pid;
+    init_dev();
+    init_tty();
 
-  init_dev();
-  init_tty();
-  
-  pid = vfork();
-  if (pid == 0)
-  {
-    execv(shell_path, shell_argv);
-    return 1;
-  }
-
-  // delete bash after execving it
-  write_dummy_file();
-  
-  start_init_routine();
-  return 0;
+    
+    run_unit_test();
+    run_shell();
+    start_init_routine();
+    return 0;
 }
