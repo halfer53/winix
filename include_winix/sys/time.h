46fc6fcb9565b1ee269275e509a37786509d4c63
Bruce Tan <brucetansh@gmail.com>
2022-07-30 23:14:27 +1200

    Add syscall wrapper for setitimer

--- include_winix/sys/time.h
+++ include_winix/sys/time.h
@@ -3,6 +3,10 @@
 
 #include <sys/types.h>
 
+#define ITIMER_REAL         0
+#define ITIMER_VIRTUAL      1
+#define ITIMER_PROF         2
+
 struct timeval {
     time_t      tv_sec;         /* seconds */
     suseconds_t tv_usec;        /* microseconds */
@@ -13,6 +17,13 @@ struct itimerval {
     struct timeval it_value;    /* Time until next expiration */
 };
 
+int setitimer(int which, const struct itimerval * new_value,struct itimerval * old_value);
 
+#if defined(__wramp__) & !defined(LINTING) && !defined(_SYSTEM)
+
+#define setitimer(which, new_value, old_value) \
+    wramp_syscall(SETITIMER, which, new_value, old_value)
+
+#endif
 
 #endif
