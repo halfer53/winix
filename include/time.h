487fba5c0c0bfbbe41865dd335ac0df1e0ddb6c5
Bruce Tan <brucetansh@gmail.com>
2022-08-13 15:22:05 +1200

    add timespec

--- include/time.h
+++ include/time.h
@@ -3,7 +3,14 @@
 
 #include <sys/times.h>
 
+struct timespec {
+    time_t tv_sec;
+    long tv_nsec;
+};
+
 clock_t times(struct tms *buf);
+
+
 #if defined(__wramp__) & !defined(LINTING) && !defined(_SYSTEM)
 #define times(buf)                          wramp_syscall(TIMES, buf)
 #endif
