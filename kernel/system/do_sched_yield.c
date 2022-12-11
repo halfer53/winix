2066bdf3a8604296aac62350920ff21f50e0deb0
Bruce Tan <brucetansh@gmail.com>
2022-12-11 16:30:51 +1300

    update sched_yield

--- kernel/system/do_sched_yield.c
+++ kernel/system/do_sched_yield.c
@@ -1,7 +1,7 @@
 #include <kernel/kernel.h>
 
 int do_sched_yield(struct proc* who, struct message* m){
-    who->priority = MIN_PRIORITY;
+    who->priority = who->priority == MIN_PRIORITY ? MIN_PRIORITY : who->priority - 1;
     return 0;
 }
 
