03f5d7e15c2488db959a0c6815441713a3175c0f
Bruce Tan <brucetansh@gmail.com>
2022-12-26 00:54:54 +1300

    implement readlien in non canonical

--- lib/readline/readline.c
+++ lib/readline/readline.c
@@ -3,15 +3,20 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <termios.h>
 
 #define BUFFER_SIZ  (128)
 
 static bool rl_initied = false;
+struct termios rl_termios;
 
 void init_readline(){
-    int flags = fcntl(STDIN_FILENO, F_GETFL);
-    flags |= O_NONBLOCK;
-    fcntl(STDIN_FILENO, F_SETFL, flags);
+    tcgetattr(STDIN_FILENO, &rl_termios);
+    rl_termios.c_lflag &= ~(ICANON | ECHO);
+    rl_termios.c_cc[VMIN] = 1;
+    rl_termios.c_cc[VTIME] = 0;
+    tcsetattr(STDIN_FILENO, TCSANOW, &rl_termios);
+    rl_initied = true;
 }
 
 int rl_readchar(){
@@ -24,16 +29,21 @@ int rl_readchar(){
 
 int rl_getline(){
     int c;
+
+    rl_point = 0;
+    rl_end = 0;
+    rl_done = 0;
+    rl_line_buffer = malloc(BUFFER_SIZ);
     while(1){
         c = rl_readchar();
-        if(c == EOF)
-            return -1;
+        if (c == '\r')
+            c = '\n';
         if(c == '\n'){
             rl_line_buffer[rl_end] = '\0';
             rl_done = 1;
             return 0;
         }
-        else if (c == '\b' || c == 127){
+        else if (c == rl_termios.c_cc[VERASE]){ // Backspace or Delete
             if(rl_point > 0){
                 rl_point--;
                 rl_end--;
@@ -41,6 +51,9 @@ int rl_getline(){
                 fflush(stdout);
             }
             continue;
+        }
+        else if (c == EOF || c == rl_termios.c_cc[VEOF]) { // Control D
+            return EOF;
         }
         
         if (rl_end >= BUFFER_SIZ)
@@ -54,22 +67,15 @@ int rl_getline(){
 
 char *readline(char *prompt){
     int ret;
-    if(!rl_initied){
+    if(!rl_initied)
         init_readline();
-        rl_initied = true;
-    }
 
     rl_prompt = prompt;
-    rl_point = 0;
-    rl_end = 0;
-    rl_done = 0;
-    rl_line_buffer = malloc(BUFFER_SIZ);
-
     fwrite(rl_prompt, 1, strlen(rl_prompt), stdout);
     fflush(stdout);
     
     ret = rl_getline();
-    if (ret && !rl_end)
+    if (ret)
         return NULL;
     return rl_line_buffer;
 }
