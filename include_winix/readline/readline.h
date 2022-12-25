aed7bff593f56349d22da1d2d7c3ce72cee0509a
Bruce Tan <brucetansh@gmail.com>
2022-12-26 02:10:04 +1300

    add rl_eof_found

--- include_winix/readline/readline.h
+++ include_winix/readline/readline.h
@@ -2,12 +2,16 @@
 #define __READLINE_READLINE_H_
 
 #include <stdio.h>
+#include <termios.h>
 
 extern char * rl_line_buffer; // This is the line gathered so far. You are welcome to modify the contents of the line, but see Allowing Undoing. The function rl_extend_line_buffer is available to increase the memory allocated to rl_line_buffer.
 extern int rl_point; // The offset of the current cursor position in rl_line_buffer (the point). This is always between 0 and rl_end.
 extern int rl_end; // The number of characters present in rl_line_buffer. When rl_point is at the end of the line, rl_point and rl_end are equal.
 extern char* rl_prompt; // The prompt Readline uses. This is set from the argument to readline(), and should not be assigned to directly. The rl_set_prompt() function (see Redisplay) may be used to modify the prompt string after calling readline().
 extern int rl_done; // This is a flag that is set to 1 when readline() has finished reading a line. It is useful for applications that want to call readline() multiple times in a loop, and do something else in between calls. The application should set this to 0 before calling readline() again.
+extern int rl_eof_found; // This is a flag that is set to 1 when readline() has found EOF on the input stream. It is useful for applications that want to call readline() multiple times in a loop, and do something else in between calls. The application should set this to 0 before calling readline() again.
+
+extern struct termios rl_ttyset; // The terminal settings that Readline uses. This is set from the argument to readline(), and should not be assigned to directly. The rl_set_tty() function (see Redisplay) may be used to modify the terminal settings after calling readline().
 
 extern FILE * rl_instream; // The stdio stream from which Readline reads input.
 extern FILE * rl_outstream; // The stdio stream to which Readline performs output.
--- lib/readline/vars.c
+++ lib/readline/vars.c
@@ -7,6 +7,7 @@ int rl_point = 0; // The offset of the current cursor position in rl_line_buffer
 int rl_end = 0; // The number of characters present in rl_line_buffer. When rl_point is at the end of the line, rl_point and rl_end are equal.
 char* rl_prompt =  NULL; // The prompt Readline uses. This is set from the argument to readline(), and should not be assigned to directly. The rl_set_prompt() function (see Redisplay) may be used to modify the prompt string after calling readline().
 int rl_done = 0; // This is a flag that is set to 1 when readline() has finished reading a line. It is useful for applications that want to call readline() multiple times in a loop, and do something else in between calls. The application should set this to 0 before calling readline() again.
+int rl_eof_found = 0; // This is a flag that is set to 1 when readline() has found EOF on the input stream. It is useful for applications that want to call readline() multiple times in a loop, and do something else in between calls. The application should set this to 0 before calling readline() again.
 
 FILE * rl_instream = stdin; // The stdio stream from which Readline reads input.
 FILE * rl_outstream = stdout; // The stdio stream to which Readline performs output.
