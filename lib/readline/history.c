441d344230624869f92bbb9bfd10a3beb9a7948f
Bruce Tan <brucetansh@gmail.com>
2022-12-26 16:11:27 +1300

    Add initial impl of readline history

---
+++ lib/readline/history.c
@@ -0,0 +1,33 @@
+#include <readline/history.h>
+#include <stdlib.h>
+
+static HISTORY_STATE history_state;
+#define INITIAL_HISTORY_SIZE 10
+
+HISTORY_STATE* history_get_history_state (){
+    return &history_state;
+}
+
+void init_history(){
+    if (history_state.entries != NULL)
+        return;
+    history_state.entries = malloc(sizeof(HIST_ENTRY*) * INITIAL_HISTORY_SIZE);
+    history_state.offset = 0;
+    history_state.length = 0;
+    history_state.size = INITIAL_HISTORY_SIZE;
+    history_state.flags = 0;
+}
+
+void add_history(char *line){
+    HIST_ENTRY* entry = malloc(sizeof(HIST_ENTRY));
+    init_history();
+    entry->line = line;
+    entry->data = NULL;
+    if (history_state.length >= history_state.size){
+        history_state.size *= 2;
+        history_state.entries = realloc(history_state.entries, sizeof(HIST_ENTRY*) * history_state.size);
+    }
+    history_state.entries[history_state.length] = entry;
+    history_state.offset++;
+    history_state.length++;
+}
