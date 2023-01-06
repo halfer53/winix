133838be97be059ea504c63703875831d31eafb6
Bruce Tan <brucetansh@gmail.com>
2023-01-06 22:55:52 +1300

    refactor times

--- kernel/system/do_times.c
+++ kernel/system/do_times.c
@@ -23,18 +23,21 @@
 
 int do_times(struct proc *who, struct message *m){
     vptr_t *vp = m->m1_p1;
-    struct tms* buf;
-    if(vp && !is_vaddr_accessible(vp, who))
-        return -EFAULT;
+    struct tms buf;
+    int ret;
+    
     if(vp){
-        buf = (struct tms*)get_physical_addr(vp, who);
-        buf->tms_utime = who->time_used;
-        buf->tms_stime = who->sys_time_used;
-        buf->tms_cstime = 0;
-        buf->tms_cutime = 0;
+        if ((ret = copy_from_user(who, (ptr_t *)&buf, (vptr_t *)vp, sizeof(struct tms))) < 0)
+            return ret;
+
+        buf.tms_utime = who->time_used;
+        buf.tms_stime = who->sys_time_used;
+        buf.tms_cstime = 0;
+        buf.tms_cutime = 0;
+
+        if ((ret = copy_to_user(who, (vptr_t *)vp, (ptr_t *)&buf, sizeof(struct tms))) < 0)
+            return ret;
     }
-    // m->m2_l1 = get_uptime(); 
-    // m->m2_l2 = who->time_used;
-    // m->m2_l3 = who->sys_time_used;
+
     return get_uptime();
 }
